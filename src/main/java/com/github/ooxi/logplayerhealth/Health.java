0ba17d058940a1b6c07282ccb1df759259e24393
ooxi <violetland@mail.ru>
2022-11-12 14:29:53 +0100

    Now logging changes in players' health

---
+++ src/main/java/com/github/ooxi/logplayerhealth/Health.java
@@ -0,0 +1,6 @@
+// Copyright 2022 - 2022, ooxi and the log-player-health contributors
+// SPDX-License-Identifier: libpng-2.0
+package com.github.ooxi.logplayerhealth;
+
+public record Health(double current, double max) {
+}
